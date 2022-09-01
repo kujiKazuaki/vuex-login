f078cacec6808705fc72316d49d8d7985c8e226b
kujiKazuaki <KujiKazuaki24@gmail.com>
2022-09-01 19:01:24 +0900

    vuexやり方分かった！

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="allContainer">
+    <button v-on:click="change()">変更</button>
+    <div>{{ this.$store.state.count }}</div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  methods: {
+    change() {
+      this.$store.state.count += 1
+    },
+  },
 }
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+.allContainer {
+  border: 2px solid red;
 }
 </style>
